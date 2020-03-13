62d022045bb72533c3c65acde7f066f2f26d1a21
Nico Man <nicoman329@outlook.com>
2020-03-13 23:24:06 +0000

    Simplied the code, used Plotly instead of OSMNX

--- contributions/Type01_Points/Type01_points.py
+++ contributions/Type01_Points/Type01_points.py
@@ -1,51 +1,32 @@
-import networkx as nx
 import csv
-import pandas as pd
-import osmnx as ox
-import requests
-import matplotlib.pyplot as plt
-import matplotlib.cm as cm
-import matplotlib.colors as colors
-inner_london_boroughs = ['City and County of the City of London',
-                             'London Borough of Camden, London',
-                             'Royal Borough of Greenwich, London',
-                             'London Borough of Hackney, London',
-                             'London Borough of Hammersmith and Fulham, London',
-                             'London Borough of Islington, London',
-                             'Royal Borough of Kensington and Chelsea, London',
-                             'London Borough of Lambeth, London',
-                             'London Borough of Lewisham, London',
-                             'London Borough of Southwark, London',
-                             'London Borough of Tower Hamlets, London',
-                             'London Borough of Wandsworth, London',
-                             'City of Westminster, London']
+import plotly.express as px
+import plotly.graph_objects as go
 
-
-longitude = []
-latitude = []
-with open('Pubs.csv', 'r') as long_lat:
-    reader = csv.reader(long_lat)
+long_list = []
+lat_list = []
+label = []
+with open('Pubs.csv', 'r') as file:
+    reader = csv.reader(file)
     next(reader, None)
     for row in reader:
-        longitude.append(row[10])
-        latitude.append(row[11])
+        long_list.append((row[10]))
+        lat_list.append((row[11]))
+        label.append(row[0])
 
-G = ox.graph_from_place('London Borough of Southwark, London')
-fig, ax = ox.plot_graph(G, show=False, close=False, node_size=1)
-ax.scatter(-0.071038176, 51.4735944, s=20, c='red')
-plt.show()
+mapbox_access_token = 'pk.eyJ1Ijoibmljb2xlbGVtYW4iLCJhIjoiY2s3cXJoeWRzMDVweTNvcXBoZHlkdWxnbCJ9.v5ZpKMG5A1_i8svZ03aSvg'
+fig = go.Figure(go.Scattermapbox(lat=lat_list, lon=long_list, mode='markers',
+        marker=go.scattermapbox.Marker(size=5), text=label,))
 
-'''
-Testing code
-'''
-# G = ox.graph_from_address('1600 Pennsylvania Ave NW, Washington, DC 20500', distance=1000)
-# fig, ax = ox.plot_graph(G, show=False, close=False)
-# ax.scatter(-77.036498, 38.897270, c='red')
-# plt.show()
+fig.update_layout(title='Location of pubs in Central London', autosize=True, hovermode='closest',
+    mapbox=dict(
+        accesstoken=mapbox_access_token,
+        bearing=0,
+        center=dict(lat=51.5,
+            lon=-0.12),
+        pitch=0,
+        zoom=10
+    ),
+)
 
-# G = ox.graph_from_place('London Borough of Southwark, London' , network_type='drive', retain_all=True)
-# pub = ox.get_nearest_node(G, (-0.071038176, 51.4735944))
-# ax.scatter(-0.071038176, 51.4735944, c='red')
-# fig, ax = ox.plot_graph(G, show=False, close=False)
-# ox.get_nearest_node(G, (39.982066, -81.11861))
-# ox.plot_graph_route(G, [ox.get_nearest_node(G, (39.982066, -81.11861))])
+fig.show()
+
