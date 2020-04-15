12b723a6762a80be3e852e4665bfb4f16673933f
nicoleleman <nicoman329@outlook.com>
2020-04-15 22:57:09 +0100

    Updating Type 2 maps

--- contributions/Type01_Points/Type01_points.py
+++ contributions/Type01_Points/Type01_points.py
@@ -17,18 +17,36 @@ with open('Pubs.csv', 'r') as file:
 
 mapbox_access_token = 'pk.eyJ1Ijoibmljb2xlbGVtYW4iLCJhIjoiY2s3cXJoeWRzMDVweTNvcXBoZHlkdWxnbCJ9.v5ZpKMG5A1_i8svZ03aSvg'
 fig = go.Figure(go.Scattermapbox(lat=lat_list, lon=long_list, mode='markers',
-        marker=go.scattermapbox.Marker(size=5), text=label,))
+        marker=go.scattermapbox.Marker(size=7), text=label,))
 
 fig.update_layout(title='Location of pubs in Central London', autosize=True, hovermode='closest',
     mapbox=dict(
         accesstoken=mapbox_access_token,
         bearing=0,
-        center=dict(lat=51.5,
-            lon=-0.12),
+        center=dict(lat=51.514,
+            lon=-0.093),
         pitch=0,
-        zoom=10
+        zoom=14
     ),
 )
 
 fig.show()
 
+from folium import plugins
+
+# # let's start again with a clean copy of the map of San Francisco
+# sanfran_map = folium.Map(location = [latitude, longitude], zoom_start = 12)
+#
+# # instantiate a mark cluster object for the incidents in the dataframe
+# incidents = plugins.MarkerCluster().add_to(sanfran_map)
+#
+# # loop through the dataframe and add each data point to the mark cluster
+# for lat, lng, label, in zip(df_incidents.Y, df_incidents.X, df_incidents.Category):
+#     folium.Marker(
+#         location=[lat, lng],
+#         icon=None,
+#         popup=label,
+#     ).add_to(incidents)
+#
+# # display map
+# sanfran_map
--- contributions/Type02_Lines/Type02_Lines.py
+++ contributions/Type02_Lines/Type02_Lines.py
@@ -1,3 +1,15 @@
-import csv
-import plotly.express as px
-import plotly.graph_objects as go
+import osmnx as ox
+from IPython.display import Image
+
+# configure the inline image display
+def create_map():
+    img_folder = 'images'
+    extension = 'png'
+    size = 300
+    dpi = 150
+    place = 'london'
+    point = (51.510244, -0.138130)
+    fig, ax = ox.plot_figure_ground(point=point, filename=place, dpi=dpi)
+    Image('{}.{}'.format(place, extension), height=size, width=size)
+
+create_map()
