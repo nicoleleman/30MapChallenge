b435502ecd0828b1d14b665d4ba18ec83d676686
Nico Man <nicoman329@outlook.com>
2020-04-23 23:58:55 +0100

    Update to Type03_Polygons.py

--- contributions/Type03_Polygons/Type03_Polygons.py
+++ contributions/Type03_Polygons/Type03_Polygons.py
@@ -4,14 +4,22 @@ import numpy as np
 import pandas as pd
 import plotly.express as px
 
-with urlopen("https://martinjc.github.io/UK-GeoJSON/json/eng/topo_eer.json") as response:
-    boroughs = json.load(response)
+with open('Counties_and_Unitary_Authorities_December_2016_Full_Extent_Boundaries_in_England_and_Wales.geojson') as response:
+    counties = json.load(response)
 
-fig = px.choropleth(geojson=boroughs,
+df = pd.read_csv("Average-prices-2020-02.csv", dtype={"Area_Code": str})
+data = df[['Area_Code','Average_Price']]
+
+fig = px.choropleth_mapbox(data, geojson=counties,
+                    locations = 'Area_Code',
+                    color = 'Average_Price',
                     color_continuous_scale="Viridis",
-                    range_color=(0, 12),
-                    scope="europe"
+                    range_color=(0, 1500000),
+                    mapbox_style="carto-positron",
+                    zoom = 5,
+                    center={"lat": 53.1105, "lon": -1.1380},
+                    labels={'Average_Price': 'Average Housing Price (£)'}
                     )
 fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-
 fig.show()
+
---
+++ contributions/Type04_Hexagons/Type04_Hexagons.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.cm as cm
+import cmocean #  http://matplotlib.org/cmocean/
+import plotly.graph_objs as go
+
